4e4ee3ac6ad0c8a9ac6e4afa200849400a04a88f
m_fomicheva <mari.fomicheva@gmail.com>
2020-06-23 16:44:20 +0200

    Save predictions and correlation

--- predict.py
+++ predict.py
@@ -1,4 +1,5 @@
 import argparse
+import os
 import torch
 
 from transformers import AutoTokenizer, AutoModel
@@ -17,14 +18,16 @@ def predict(args):
     model.load_state_dict(torch.load(args.model_path))
     model.to(gpu)
     model.eval()
-
     dataset = QEDataset(
         args.test_file, args.test_mt_file, args.test_wp_file, features_path=args.test_features_file)
     predicted_scores, pearson, mse = evaluate(
         dataset, model, tokenizer, args.batch_size, gpu, num_features=args.num_features, encode_separately=args.encode_separately,
         use_word_probs=args.use_word_probs, get_metrics=True)
-    print(predicted_scores)
-    print(pearson)
+    with open(os.path.join(args.output_dir, 'predictions.tsv'), 'w') as fout:
+        for score in predicted_scores:
+            fout.write('{}\n'.format(score))
+    with open(os.path.join(args.output_dir, 'predict.correlation.log'), 'w') as fout:
+        fout.write('{}\n'.format(pearson))
 
 
 def main():
